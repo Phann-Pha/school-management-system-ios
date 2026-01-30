39800d7588a233b46026e0d6c33d94d77255fd9a
Phann Pha <phannpha.fa@gmail.com>
2026-01-30 18:57:05 +0700

    modified color res

--- school-management-system-ios/ContentView.swift
+++ school-management-system-ios/ContentView.swift
@@ -2,23 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader(content: { proxy in
+        GeometryReader(content: { _ in
             ZStack {
-                
                 // COLOR BACKGROUND
-                Color.black
-                .opacity(1)
-                .ignoresSafeArea(edges: .all)
-                
+                Color(UIColor(resource: .white))
+                    .opacity(1)
+                    .ignoresSafeArea(edges: .all)
+
                 // MAIN CONTENT
                 VStack {
                     Text("Hello, World!")
                         .font(.body)
-                        .foregroundStyle(.white)
+                        .foregroundStyle(Color(UIColor(resource: .black)))
                 }
                 .frame(maxWidth: .infinity, alignment: .center)
                 .padding(.vertical, 24)
-                
             }
         })
     }
