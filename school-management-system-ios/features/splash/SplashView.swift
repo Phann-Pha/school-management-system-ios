9ae217bbb1a1133650ae8f71def35d237d13b022
Phann Pha <phannpha.fa@gmail.com>
2026-02-06 13:18:20 +0700

    removed animation fade.

--- school-management-system-ios/features/splash/SplashView.swift
+++ school-management-system-ios/features/splash/SplashView.swift
@@ -2,8 +2,6 @@
 import SwiftUI
 
 struct SplashView: View {
-    @State private var scale = 0.5
-    @State private var opacity = 0.0
     @ObservedObject var coordinator: AppCoordinator
 
     var body: some View {
@@ -18,25 +16,9 @@ struct SplashView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: size, height: size)
-                        .scaleEffect(scale)
-                        .opacity(opacity)
-                        .onAppear {
-                            withAnimation(.easeIn(duration: 1.2)) {
-                                self.scale = 1.0
-                                self.opacity = 1.0
-                            }
-                        }
 
                     Spacer()
                     Text("1.0.0").font(.system(size: 13, weight: .none))
-                        .scaleEffect(scale)
-                        .opacity(opacity)
-                        .onAppear {
-                            withAnimation(.easeIn(duration: 1.2)) {
-                                self.scale = 1.0
-                                self.opacity = 1.0
-                            }
-                        }
                 }
             }
             .onAppear {
