8e5a429bd523f2ec2f54fd82bd43b25656011855
Phann Pha <phannpha.fa@gmail.com>
2026-02-16 22:44:20 +0700

    click animation button.

--- school-management-system-ios/features/splash/SelectingLanguageView.swift
+++ school-management-system-ios/features/splash/SelectingLanguageView.swift
@@ -2,41 +2,53 @@ import Lottie
 import SwiftUI
 
 struct SelectingLanguageView: View {
-    @State private var is_back: Bool = false
+    @State private var is_en: Bool = true
+    @State private var is_kh: Bool = false
 
     var body: some View {
-        GeometryReader { proxy in
-            ZStack {
-                Color(UIColor(resource: .white))
-                    .opacity(1)
-                    .ignoresSafeArea(.all)
+        NavigationStack {
+            GeometryReader { proxy in
+                ZStack {
+                    Color(UIColor(resource: .white))
+                        .opacity(1)
+                        .ignoresSafeArea(.all)
 
-                VStack(alignment: .center) {
-                    HeaderContentView()
-                    Spacer()
-                    ContentView(size: proxy.size.width * 0.7)
-                    Spacer()
-                    FooterContentView()
+                    VStack(alignment: .center) {
+                        HeaderContentView() {
+                            // back here
+                        }
+                        Spacer()
+                        ContentView(size: proxy.size.width * 0.7)
+                        Spacer()
+                        FooterContentView()
+                    }
                 }
             }
+            .navigationDestination(for: String.self, destination: { _ in
+                GettingStartedView()
+            })
         }
     }
 
     @ViewBuilder
-    private func HeaderContentView() -> some View {
+    private func HeaderContentView(callback: @escaping () -> Void) -> some View {
         HStack {
+            ButtonBack(callback: callback)
+            Spacer()
+        }
+        .padding([.top, .leading, .trailing], 24)
+    }
+
+    @ViewBuilder
+    private func ButtonBack(callback: @escaping () -> Void) -> some View {
+        Button(action: callback) {
             Image(systemName: "arrow.left")
                 .resizable()
                 .padding(10)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40)
-                .onTapGesture(perform: {
-                    is_back.toggle()
-                })
-
-            Spacer()
-        }
-        .padding([.top, .leading, .trailing], 24)
+            
+        }.buttonStyle(AnimatedButtonStyle())
     }
 
     @ViewBuilder
@@ -62,23 +74,55 @@ struct SelectingLanguageView: View {
             .padding(.horizontal, 24)
             .padding(.bottom, 32)
 
-            GetStartedButton(text: "ភាសារខ្មែរ", isSelected: true)
-            GetStartedButton(text: "English", isSelected: false)
+            ButtonKhmer(isSelected: is_kh) {
+                is_kh = true
+                is_en = false
+            }
+            ButtonEnglish(isSelected: is_en) {
+                is_en = true
+                is_kh = false
+            }
         }
+        .padding(.horizontal, 24)
         .padding(.bottom, 45)
     }
 
     @ViewBuilder
-    private func GetStartedButton(text: String, isSelected: Bool) -> some View {
-        Button(action: { }, label: {
-            Text(text)
+    private func ButtonKhmer(isSelected: Bool, callback: @escaping () -> Void) -> some View {
+        Button(action: callback) {
+            Text("ភាសាខ្មែរ")
                 .font(.system(size: 16, weight: .medium))
-                .frame(maxWidth: .infinity, maxHeight: 60)
+                .frame(maxWidth: .infinity)
+                .frame(height: 60)
                 .foregroundColor(Color(UIColor(resource: isSelected ? .white : .black)))
                 .background(Color(UIColor(resource: isSelected ? .primary : .white)))
-                .cornerRadius(12)
-                .padding(.horizontal, 24)
-        })
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+
+        }.buttonStyle(AnimatedButtonStyle())
+    }
+
+    @ViewBuilder
+    private func ButtonEnglish(isSelected: Bool, callback: @escaping () -> Void) -> some View {
+        Button(action: callback) {
+            Text("English")
+                .font(.system(size: 16, weight: .medium))
+                .frame(maxWidth: .infinity)
+                .frame(height: 60)
+                .foregroundColor(Color(UIColor(resource: isSelected ? .white : .black)))
+                .background(Color(UIColor(resource: isSelected ? .primary : .white)))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+
+        }.buttonStyle(AnimatedButtonStyle())
+    }
+}
+
+struct AnimatedButtonStyle: ButtonStyle {
+    var cornerRadius: CGFloat = 16.0
+    func makeBody(configuration: Configuration) -> some View {
+        return configuration.label
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+            .animation(.linear(duration: 0.1), value: configuration.isPressed)
     }
 }
 
