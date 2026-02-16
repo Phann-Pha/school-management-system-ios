9bd25797b2cbcfb43e0c9538b4cbec6341331e38
Phann Pha <phannpha.fa@gmail.com>
2026-02-16 18:39:01 +0700

    clean code button get started.

--- school-management-system-ios/features/splash/GettingStartedView.swift
+++ school-management-system-ios/features/splash/GettingStartedView.swift
@@ -1,7 +1,9 @@
-import SwiftUI
 import Lottie
+import SwiftUI
 
 struct GettingStartedView: View {
+    @State private var btnGetStarted: Bool = false
+
     var body: some View {
         GeometryReader { proxy in
             let size = proxy.size.width * 0.7
@@ -29,16 +31,28 @@ struct GettingStartedView: View {
                         .padding(.horizontal, 24)
                         .padding(.bottom, 32)
 
-                        SplashButtonView(text: "Get Started")
+                        GetStartedButton(text: "Get Started")
                     }
                     .padding(.bottom, 45)
                 }
             }
         }
     }
+
+    @ViewBuilder
+    private func GetStartedButton(text: String) -> some View {
+        Button(action: { btnGetStarted.toggle() }, label: {
+            Text(text)
+                .font(.system(size: 16, weight: .medium))
+                .frame(maxWidth: .infinity, maxHeight: 60)
+                .foregroundColor(Color(UIColor(resource: .white)))
+                .background(Color(UIColor(resource: .primary)))
+                .cornerRadius(12)
+                .padding(.horizontal, 24)
+        })
+    }
 }
 
 #Preview {
     GettingStartedView()
 }
-
