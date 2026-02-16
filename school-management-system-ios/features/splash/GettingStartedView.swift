1a1e583bde6a9c222684e184345f731c619d6f21
Phann Pha <phannpha.fa@gmail.com>
2026-02-16 18:52:46 +0700

    splited code by content view.

--- school-management-system-ios/features/splash/GettingStartedView.swift
+++ school-management-system-ios/features/splash/GettingStartedView.swift
@@ -2,7 +2,8 @@ import Lottie
 import SwiftUI
 
 struct GettingStartedView: View {
-    @State private var btnGetStarted: Bool = false
+    @State private var is_skip: Bool = false
+    @State private var is_clicked_get_started: Bool = false
 
     var body: some View {
         GeometryReader { proxy in
@@ -10,38 +11,62 @@ struct GettingStartedView: View {
             ZStack {
                 Color(UIColor(resource: .white)).ignoresSafeArea(edges: .all)
                 VStack(alignment: .center) {
+                    HeaderContentView()
                     Spacer()
-
-                    VStack(alignment: .center) {
-                        LottieView(animation: .named("exam_anim.json"))
-                            .playing(loopMode: .loop)
-                    }
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: size, height: size)
-
+                    ContentView(size: size)
                     Spacer()
-                    VStack(alignment: .center) {
-                        HStack {
-                            Text("Your smart solution for fast, accurate attendence tracking. Let's a quick tour!")
-                                .multilineTextAlignment(.center)
-                                .font(.system(size: 16, weight: .regular))
-                                .foregroundColor(Color(UIColor(resource: .black)))
-                                .lineSpacing(8)
-                        }
-                        .padding(.horizontal, 24)
-                        .padding(.bottom, 32)
-
-                        GetStartedButton(text: "Get Started")
-                    }
-                    .padding(.bottom, 45)
+                    FooterContentView()
                 }
             }
         }
     }
 
     @ViewBuilder
+    private func HeaderContentView() -> some View {
+        HStack {
+            Spacer()
+            Text("Skip")
+                .font(.system(size: 16, weight: .regular))
+                .foregroundColor(Color(UIColor(resource: .black)))
+                .lineLimit(1)
+                .onTapGesture(perform: {
+                    is_skip.toggle()
+                })
+        }
+        .padding([.top, .leading, .trailing], 24)
+    }
+
+    @ViewBuilder
+    private func ContentView(size: CGFloat) -> some View {
+        VStack(alignment: .center) {
+            LottieView(animation: .named("exam_anim.json"))
+                .playing(loopMode: .loop)
+        }
+        .aspectRatio(contentMode: .fit)
+        .frame(width: size, height: size)
+    }
+
+    @ViewBuilder
+    private func FooterContentView() -> some View {
+        VStack(alignment: .center) {
+            HStack {
+                Text("Your smart solution for fast, accurate attendence tracking. Let's a quick tour!")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 16, weight: .regular))
+                    .foregroundColor(Color(UIColor(resource: .black)))
+                    .lineSpacing(8)
+            }
+            .padding(.horizontal, 24)
+            .padding(.bottom, 32)
+
+            GetStartedButton(text: "Get Started")
+        }
+        .padding(.bottom, 45)
+    }
+
+    @ViewBuilder
     private func GetStartedButton(text: String) -> some View {
-        Button(action: { btnGetStarted.toggle() }, label: {
+        Button(action: { is_clicked_get_started.toggle() }, label: {
             Text(text)
                 .font(.system(size: 16, weight: .medium))
                 .frame(maxWidth: .infinity, maxHeight: 60)
